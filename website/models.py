4e3b26cf4e02409ebef7fd0d323e2eed9ca2bca6
georgi-stoyanow <gemast1311@gmail.com>
2024-06-16 01:41:24 +0300

    Revert "login & signup"
    This reverts commit 24ad65b1c979b272636418931b4ecd49efc070e8.

--- website/models.py
+++ website/models.py
@@ -7,8 +7,3 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
-
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
